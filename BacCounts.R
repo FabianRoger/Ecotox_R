0d0172101c2c0bbcf093ecbb7735bb39a75e6a65
Fabian Roger <Fabian_Roger@gmx.de>
2015-04-27 23:07:01 +0200

    calculated maximum biomass

--- BacCounts.R
+++ BacCounts.R
@@ -28,11 +28,39 @@ Bac[Bac$sampling >= ymd(20131010),]$ToxYN <-"YES"
 
 # plot Cell counts by diversity and Tox
 
-ggplot(Bac, aes(y=CellsT, x=sampling,colour=ToxYN))+
+ggplot(Bac[Bac$DIV != "S",], aes(y=CellsT, x=sampling,colour=ToxYN))+
   facet_wrap(~DIV*Tox,ncol=7)+
   stat_summary(fun.y = mean, geom = "line",linetype="solid")+
-  stat_summary(aes(y=CellsL),fun.y = mean, geom = "line",linetype="dashed")+
-  stat_summary(aes(y=CellsD),fun.y = mean, geom = "line",linetype="dotted")+
+  #stat_summary(aes(y=CellsL),fun.y = mean, geom = "line",linetype="dashed")+
+  #stat_summary(aes(y=CellsD),fun.y = mean, geom = "line",linetype="dotted")+
   theme_bw()+
   scale_colour_manual(values=c("darkgreen","darkred"))
-  
+
+
+# calculate max diversity for both with and without toxin as 
+# mean of two highest values
+
+maxBac<-ddply(Bac[,c("CellsT","DIV","Tox","BOT","ToxC","ToxYN")],
+              .(DIV,Tox,BOT,ToxYN,ToxC
+                ), function(x) mean(x[with(x, order(-CellsT)),][1:2,]$Cells))
+
+ggplot(maxBac[maxBac$DIV != "S",], aes(y=V1, x=DIV,colour=ToxYN,group=ToxYN))+
+  facet_wrap(~ToxYN*Tox,nrow=2)+
+  stat_summary(fun.y = mean, geom = "point")+
+  stat_smooth(method="lm")+
+  labs(y="maximum Biomass", x="Diversity level")+
+  theme_bw(base_size=15)+
+  scale_colour_manual(values=c("darkgreen","darkred"))
+
+ggplot(maxBac[maxBac$DIV != "S" & maxBac$ToxYN == "YES",], aes(y=V1, x=Tox,group=DIV))+
+  facet_wrap(~DIV,nrow=1)+
+  stat_summary(fun.y = mean, geom = "point")+
+  stat_smooth(method="lm")+
+  labs(y="maximum Biomass", x="Toxicity level")+
+  theme_bw(base_size=15)+
+  scale_colour_manual(values=c("darkgreen","darkred"))
+
+
+
+
+
